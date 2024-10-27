9430998d0f6da98b359309c8f09f45cd204df113
Ambroise Berenger <aberenge@c2r12p2.42lehavre.fr>
2024-10-27 03:41:43 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 03:40:28 by aberenge          #+#    #+#             */
+/*   Updated: 2024/10/27 03:40:30 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 6
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_trim_storage(char *storage);
+char	*ft_getline(char *storage);
+char	*ft_readline(int fd, char *storage);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlcpy(char *dst, char *src, size_t size);
+size_t	ft_strlen(char *str);
+
+#endif
